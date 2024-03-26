2c090e21cb3dabbee2837d6b6328fa99008e2283
PhamDinhMinh <phamdinhminh030801@gmail.com>
2024-03-26 23:16:29 +0700

    feat: add base init web admin

---
+++ src/configs/constant.config.ts
@@ -0,0 +1,11 @@
+export const APP_NAME = process.env.NEXT_PUBLIC_APP_NAME;
+export const URL = process.env.NEXT_PUBLIC_URL;
+export const API_ENDPOINT = process.env.NEXT_PUBLIC_API_ENDPOINT;
+export const ACCESS_TOKEN_KEY = "accessToken";
+export const GOOGLE_MAP_API_KEY = process.env.NEXT_PUBLIC_GOOGLE_MAP_API_KEY || "";
+export const TINIFY_API_KEY = process.env.NEXT_PUBLIC_TINIFY_API_KEY || "";
+export const DEFAULT_POSITION = {
+  lat: 20.98161,
+  lng: 105.753957,
+};
+export const DaysWeek = ["CN", "T2", "T3", "T4", "T5", "T6", "T7"];
